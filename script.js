dab36fa8fa3ae992378a91c96618f8ce3a2ee28b
Nahin Hossain <1521926@iub.edu.bd>
2018-10-10 03:29:14 +0000

    nhs

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 /* Task 1 */
+ var heading = document.getElementById('heading')
 
+console.log(heading.innerText);
+
+heading.innerText = '<h3>Asa Cat!</h3>'
 
 /* Task 2 using plain JavaScript */
+var textBtn = document.getElementById('addText2');
+textBtn.addEventListener('click', function(event){
+    var task2 = document.getElementById('task2a');
+    
+    var paragraph = document.createElement('p');
+     
+    paragraph.innerText = 'Hello World!';
+    
+    task2.appendChild(paragraph);
+});
 
+
+function changeBGcolor(event)
+{
+    var body = document.getElementsByTagName('body')[0];
+    // if(event.target.innerText == 'Red')
+    // {
+    //     body.style.background = 'red';
+    // }
+    // if(event.target.innerText == 'Green')
+    // {
+    //     body.style.background = 'green';
+    // }
+    
+    body.style.backgroundColor = event.target.innerText.toLowerCase();
+}
 
 
 /* Task 4 using jQuery */
